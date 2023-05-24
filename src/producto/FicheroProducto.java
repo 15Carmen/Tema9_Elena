15534ba1cbd9645460c90c38bcadb3c3513922fe
15Carmen <carmen.martin@iesnervion.es>
2023-05-24 23:43:39 +0200

    de locos

--- src/producto/FicheroProducto.java
+++ src/producto/FicheroProducto.java
@@ -11,25 +11,44 @@ import java.util.TreeSet;
 
 public class FicheroProducto {
 
+    /**
+     * Método que lee el fichero productos.txt y almacena en un TreeSet los datos que ha leído
+     * @return TreeSet con los datos leídos del fichero
+     */
     public static TreeSet<Producto> leerFichero() {
-        TreeSet<Producto> productos = new TreeSet<>();
-        Producto p;
-        String nombre;
-        double precio;
+
+        //Declaramos la colección donde almacenaremos los productos
+        TreeSet<Producto> productosLeidos = new TreeSet<>();
+
+        //Declaramos el resto de variables que vamos a necesitar
+        Producto p;     //Variable donde almacenaremos el producto
+        String nombre;  //Variable donde almacenaremos el nombre del producto
+        double precio;  //Variable donde almacenaremos el precio del producto
+        String[] datos; //Array donde almacenaremos los datos de cada línea del fichero
+
+        //Declaramos el objeto BufferedReader para leer el fichero
         BufferedReader br = null;
         try {
-            br = new BufferedReader(new FileReader("src\\ejercicio5\\productos.txt"));
+            //Inicializamos el objeto BufferedReader con el fichero productos.txt
+            br = new BufferedReader(new FileReader("src/producto/productos.txt"));
+            //Leemos la primera línea del fichero
             String linea = br.readLine();
-            String[] datos;
+
+            //Mientras que la línea no sea nula
             while (linea != null) {
+                //Separamos los datos de la línea por el espacio en blanco
                 datos = linea.split(" ");
 
+                //Asignamos los datos a las variables
                 nombre = datos[0];
                 precio = Double.parseDouble(datos[1]); // Devuelve double, tipo primitivo
 
+                //Creamos el objeto Producto con los datos leídos del fichero
                 p = new Producto(nombre, precio);
-                productos.add(p);
+                //Añadimos el producto a la colección
+                productosLeidos.add(p);
 
+                //Leemos la siguiente línea del fichero
                 linea = br.readLine();
             }
 
@@ -50,25 +69,37 @@ public class FicheroProducto {
             }
         }
 
-        return productos;
+        return productosLeidos;
 
     }
 
-    public static void escribirFichero(TreeSet<Producto> productos) {
-        BufferedWriter bw = null;
-        String nombre;
-        double precio;
-        try {
-            bw = new BufferedWriter(new FileWriter("src\\ejercicio5\\productos.txt"));
+    /**
+     * Método que escribe en el fichero productos.txt los datos que le pasamos por parámetro
+     * @param productosEscritos TreeSet con los datos que queremos escribir en el fichero
+     */
+    public static void escribirFichero(TreeSet<Producto> productosEscritos) {
+        //Declaramos las variables que vamos a necesitar
+        String nombre;  //Variable donde almacenaremos el nombre del producto
+        double precio;  //Variable donde almacenaremos el precio del producto
 
-            for(Producto p : productos) {
+        //Declaramos el objeto BufferedWriter para escribir en el fichero
+        BufferedWriter bw = null;
+        try {
+            //Inicializamos el objeto BufferedWriter con el fichero productos.txt
+            bw = new BufferedWriter(new FileWriter("src/producto/productos.txt"));
+
+            //Recorremos el TreeSet con los productos que queremos escribir en el fichero
+            for(Producto p : productosEscritos) {
+                //Asignamos los datos de cada producto a las variables
                 nombre = p.getNombre();
                 precio = p.getPrecio();
 
+                //Escribimos en el fichero los datos del producto
                 bw.write(nombre + " " + precio);
+                //Escribimos un salto de línea
                 bw.newLine();
             }
-
+            //Limpiamos el búfer
             bw.flush();
 
         } catch (IOException e) {
--- src/producto/PrincipalProducto.java
+++ src/producto/PrincipalProducto.java
@@ -7,27 +7,38 @@ public class PrincipalProducto {
 
     public static void main(String[] args) {
 
+        //Declaramos la colección, en este caso un TreeSet de objetos Producto y le asignamos el valor devuelto por el método leerFichero()
         TreeSet<Producto> productos = FicheroProducto.leerFichero();
 
-        Producto p;
-        int opc;
-        String nombre;
-        double precio;
+        //Declaramos el resto de variables que vamos a necesitar
+        Producto p;     //Variable donde almacenaremos el producto
+        int opc;        //Variable donde almacenaremos la opción que elija el usuario
+        String nombre;  //Variable donde almacenaremos el nombre del producto
+        double precio;  //Variable donde almacenaremos el precio del producto
+
+        //Creamos el objeto Scanner para leer por teclado
         Scanner sc = new Scanner(System.in);
 
+        //Mientras que la opción elegida sea diferente de 0 (salir)
         do {
+            //Mostramos el menú y leemos la opción elegida por el usuario
             menu();
             opc = sc.nextInt();
             sc.nextLine();
 
+            //Según la opción elegida por el usuario
             switch (opc) {
                 case 1 -> {// Dar de alta el producto
+                    //Le pedimos al usuario los datos del producto
                     System.out.println("Introduzca el nombre del producto:");
                     nombre = sc.nextLine();
                     System.out.println("Introduzca el precio del producto:");
                     precio = sc.nextDouble();
                     sc.nextLine();
+                    //Creamos el objeto Producto con los datos introducidos por el usuario
                     p = new Producto(nombre, precio);
+
+                    //Si el producto no existe en la colección lo añadimos, en caso contrario mostramos un mensaje de error
                     if (productos.add(p)) {
                         System.out.println("Añadido correctamente");
                     } else {
@@ -35,9 +46,12 @@ public class PrincipalProducto {
                     }
                 }
                 case 2 -> {// Baja de producto
+                    //Le pedimos al usuario el nombre del producto
                     System.out.println("Introduzca el nombre del producto:");
                     nombre = sc.nextLine();
+                    //Creamos el objeto Producto con el nombre introducido por el usuario
                     p = new Producto(nombre);
+                    //Si el producto existe en la colección lo eliminamos, en caso contrario mostramos un mensaje de error
                     if (productos.remove(p)) {
                         System.out.println("Eliminado exitosamente");
                     } else {
@@ -45,12 +59,16 @@ public class PrincipalProducto {
                     }
                 }
                 case 3 -> { // Listar
+                    //Recorremos la colección y mostramos los productos
                     for (Producto prod : productos) {
                         System.out.println(prod);
                         System.out.println();
                     }
                 }
-                case 4 -> FicheroProducto.escribirFichero(productos);
+                case 4 -> { // Guardar
+                    //Llamamos al método escribirFichero() para guardar los productos en el fichero
+                    FicheroProducto.escribirFichero(productos);
+                }
                 case 0 -> // Salir
                         System.out.println("Saliendo del sistema...");
                 default -> System.out.println("Opción no válida");
@@ -58,9 +76,13 @@ public class PrincipalProducto {
 
         } while (opc != 0);
 
+        //Cerramos el objeto Scanner
         sc.close();
     }
 
+    /**
+     * Método que muestra el menú por pantalla
+     */
     private static void menu() {
         System.out.println("Seleccione una opción");
         System.out.println("1. Alta producto");
